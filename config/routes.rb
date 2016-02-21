08b6ffe8c2f1c49b400a41e6825349441aeb6b6f
Daniel Luz <daniel_almeida_luz@hotmail.com>
2016-02-21 20:22:28 -0300

    Just adjusting line order

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
-  get 'my_cart'               =>  'carts#show'
-
   root  'static_pages#home'
+
+  get 'my_cart'               =>  'carts#show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

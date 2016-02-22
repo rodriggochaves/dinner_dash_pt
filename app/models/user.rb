772c05c4b806d43202f3ea343b60a487cde0062c
Daniel Luz <daniel_almeida_luz@hotmail.com>
2016-02-21 22:40:05 -0300

    added some validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+
+  validates :name, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
+
+  validates :display_name, length: { minimum: 2, maximum: 32 }, 
+            allow_nil: true
 end

a2cd4d89643ebddf7a25538bf65e4d680da66434
Daniel Luz <daniel_almeida_luz@hotmail.com>
2016-02-22 23:09:31 -0300

    Added user profile page and controller show action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,14 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       #Valid save user
+      redirect_to @user
     else
       render 'new'
     end
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   private

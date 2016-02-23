9654c3c1ef26d16456f1a77ec434c53fe43bec7f
Daniel Luz <daniel_almeida_luz@hotmail.com>
2016-02-23 17:06:09 -0300

    Adjusted user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160223195406) do
+ActiveRecord::Schema.define(version: 20160223200348) do
 
   create_table "items", force: :cascade do |t|
     t.string   "name"
@@ -41,7 +41,6 @@ ActiveRecord::Schema.define(version: 20160223195406) do
     t.string   "display_name"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.string   "password"
     t.string   "password_digest"
   end
 

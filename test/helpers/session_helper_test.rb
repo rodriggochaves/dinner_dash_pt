bef6fce1bbf945bac776d84821b480e9477c3627
Daniel Luz <daniel_almeida_luz@hotmail.com>
2016-02-23 16:21:10 -0300

    Added session helper test and one user fixture

---
+++ test/helpers/session_helper_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'          
+                               
+class SessionsHelperTest < ActionView::TestCase
+
+  def setup                    
+    @user = users(:michael)    
+  end                          
+
+  test "current_user returns right user when session is nil" do
+    assert_equal @user, current_user
+    assert is_logged_in?       
+  end
+
+end
+
